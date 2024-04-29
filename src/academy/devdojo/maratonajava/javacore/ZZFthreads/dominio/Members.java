c199b8d65e1d1d02aadccde45172f65029741215
João Lourenço <joaodslourenco@gmail.com>
2024-04-29 08:11:11 -0300

    feat: add ThreadTest01

---
+++ src/academy/devdojo/maratonajava/javacore/ZZFthreads/dominio/Members.java
@@ -0,0 +1,50 @@
+package academy.devdojo.maratonajava.javacore.ZZFthreads.dominio;
+
+import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
+
+public class Members {
+    private final Queue<String> emails = new ArrayBlockingQueue<>(10);
+    private boolean open = true;
+
+    public boolean isOpen() {
+        return open;
+    }
+
+    public int pendingEmails() {
+        synchronized (this.emails) {
+            return this.emails.size();
+        }
+    }
+
+    public void addMemberEmail(String email) {
+        synchronized (this.emails) {
+            String threadName = Thread.currentThread().getName();
+            System.out.println(threadName + " adicionou email na lista.");
+            this.emails.add(email);
+            this.emails.notifyAll();
+        }
+    }
+
+    public String retrieveEmail() throws InterruptedException {
+        System.out.println(Thread.currentThread().getName() + " verificando se existem emails...");
+
+        synchronized (this.emails) {
+            while (this.emails.isEmpty()) {
+                if (!open) return null;
+                System.out.println(Thread.currentThread().getName() + " não tem email disponível na lista. Entrando em modo de espera.");
+                this.emails.wait();
+            }
+            return this.emails.poll();
+        }
+    }
+
+    public void close() {
+        open = false;
+        synchronized (this.emails) {
+            System.out.println(Thread.currentThread().getName() + " notificando todos que não estamos mais recebendo emails.");
+        }
+    }
+
+}
+
